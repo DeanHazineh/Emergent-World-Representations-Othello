bb4bf23d9a3c387c01926b50fcf0b64ec9446b93
DeanHazineh <dhazineh@g.harvard.edu>
2023-04-29 14:51:58 -0400

    Add plot function to make board visualization
    .

--- EWOthello/utils/plot_helpers.py
+++ EWOthello/utils/plot_helpers.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
 from EWOthello.data.othello import permit_reverse
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-# from matplotlib.colors import ListedColormap
-
-# colors = ["black", "white", "yellow"]
-# colormap = ListedColormap(colors)
+from matplotlib.patches import Rectangle
 
 
 def seq_to_boardCoord(sequence):
@@ -37,4 +33,29 @@ def addColorbar(thisfig, thisax, thisim=None, cbartitle=""):
     cbar.ax.get_yaxis().labelpad = 15
     cbar.ax.set_ylabel(cbartitle, rotation=90)
 
+    return
+
+
+def plot_game_discs(data_array, ax):
+    nrows, ncols = data_array.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            square = Rectangle((j - 0.5, i - 0.5), 1, 1, edgecolor="white", fill=False)
+            ax.add_artist(square)
+
+            if data_array[i, j] == 0:
+                circle_color = "black"
+            elif data_array[i, j] == 2:
+                circle_color = "white"
+            else:
+                continue
+
+            circle = plt.Circle((j, i), 0.3, color=circle_color, edgecolor="black", fill=True)
+            ax.add_artist(circle)
+            
+    ax.set_xlim(-1, ncols)
+    ax.set_ylim(-1, nrows)
+    ax.invert_yaxis()
+    ax.set_aspect("equal", "box")
+    ax.set_facecolor("gray")
     return
